d770f15d993c1b1b4aa193b68a867a3496d96f9c
estibenlicona <estibenlicona@gmail.com>
2026-02-03 22:14:05 -0500

    Remover archivos Class1.cs vacíos

--- AppStore.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AppStore.Core
-{
-    public class Class1
-    {
-    }
-}
--- AppStore.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AppStore.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
